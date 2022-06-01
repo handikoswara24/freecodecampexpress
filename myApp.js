acdd720d5593176146417f613cc76f1d2f4bd248
Handi Koswara <39146228+handikoswara24@users.noreply.github.com>
2022-06-01 13:23:18 +0000

    params

--- myApp.js
+++ myApp.js
@@ -3,7 +3,10 @@ let app = express();
 
 console.log("Hello World");
 app.use("/public",express.static(__dirname + "/public"))
-
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
 app.get("/", (req, res) => {
   //res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html")
@@ -16,6 +19,21 @@ app.get("/json", (req, res) => {
   }
   res.json({
     message
+  })
+})
+
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send({
+    time: req.time
+  })
+})
+
+app.get("/:word/echo", (req, res) => {
+  res.json({
+    echo : req.params.word
   })
 })
 
